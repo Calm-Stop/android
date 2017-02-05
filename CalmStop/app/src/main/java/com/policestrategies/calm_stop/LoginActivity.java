0f50c952e1a1ccaee3fa642805c215d59562da08
talal.abouhaiba <talal.abouhaiba@gmail.com>
2017-02-04 18:41:47 -0800

    Added citizen and officer productFlavors

---
+++ CalmStop/app/src/main/java/com/policestrategies/calm_stop/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.policestrategies.calm_stop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Allows the user to log in or create an account.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.policestrategies.calm_stop.officer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.policestrategies.calm_stop.R;
+
+/**
+ * Allows the officer to log in or create an account.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
