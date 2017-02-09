0ade12080f496d2728e028bc5362420c201da7e5
Annisa <akaraca@ucsc.edu>
2017-02-09 14:02:08 -0800

    Account creation works

--- CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupActivity.java
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.policestrategies.calm_stop.R;
 
 import static android.R.attr.password;
@@ -50,6 +51,42 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         findViewById(R.id.button_login).setOnClickListener(this);
         findViewById(R.id.button_signup).setOnClickListener(this);
 
+        // START initialize_auth so you can track when user signs in and signs out
+        mAuth = FirebaseAuth.getInstance();
+        // END initialize_auth
+
+        // START auth_state_listener
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        // END auth_state_listener
+    }
+
+    // START on_start_add_listener
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    // Remove FirebaseAuth instance on onStop()
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     /**
