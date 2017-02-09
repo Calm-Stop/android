161f2b2495b59530255f7495ca883b649a253ba0
Annisa <akaraca@ucsc.edu>
2017-02-09 12:08:45 -0800

    login activity working, create account not
    when you login with a valid email/pw combo, signInWithEmail:onComplete
    = true and OnAuthStateChanged is changed to signed_in. When you login
    with invalid email/pw combo, Validation success popup still appears,
    but signInWithEmail:onComplete is set to false

--- CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupActivity.java
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupActivity.java
@@ -2,13 +2,21 @@ package com.policestrategies.calm_stop.officer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.policestrategies.calm_stop.R;
+
+import static android.R.attr.password;
 
 /**
  * Allows the user to sign up or return to the log in page.
@@ -19,6 +27,16 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private EditText mNameField;
     private EditText mEmailField;
     private EditText mPasswordField;
+
+    private static final String TAG = "Signup";
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +66,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 break;
 
             case R.id.button_signup: // Signup was pressed, begin the SignupActivity
-                signup();
+                signup(mEmailField.getText().toString(), mPasswordField.getText().toString());
                 break;
 
         }
@@ -58,13 +76,34 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
      * Begins the signup process. Validates the input and - if input is valid - attempts to create
      * an account.
      */
-    private void signup() {
+    private void signup(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
 
         if (!validateInput()) {
             return;
         }
 
         // Now we need to attempt to signup - we'll add code for this later (once Firebase is integrated)
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+        // [END create_user_with_email]
 
         Toast.makeText(SignupActivity.this, "Validation success!", Toast.LENGTH_SHORT).show();
     }
