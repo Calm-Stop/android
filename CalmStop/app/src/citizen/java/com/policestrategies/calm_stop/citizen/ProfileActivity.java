40140add7140313602e2ff1a6f26ed5659c4858f
Maria Vizcaino <mvizcaino2@gmail.com>
2017-02-20 16:27:03 -0800

    Profile page-ish

--- CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ProfileActivity.java
+++ CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ProfileActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.policestrategies.calm_stop.R;
@@ -15,13 +18,25 @@ import com.policestrategies.calm_stop.R;
  */
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    private Spinner genderSetter;
+    private Spinner ethnicitySetter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Toast.makeText(ProfileActivity.this, "WELCOME TO THE PROFILE PAGE", Toast.LENGTH_LONG).show();
+        findViewById(R.id.backbutton).setOnClickListener(this);
+        findViewById(R.id.viewDocs).setOnClickListener(this);
+        findViewById(R.id.savebutton).setOnClickListener(this);
+
+        genderSetter = (Spinner) findViewById(R.id.genderSetter);
+        ethnicitySetter = (Spinner) findViewById(R.id.ethnicitySetter);
+
+        setUpGenderSetter();
+        setUpEthnicitySetter();
+
+
     }
 
     @Override
@@ -29,8 +44,92 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         switch(v.getId()) {
 
+            case R.id.backbutton:
+                toHomepage();
+                break;
+
+            case R.id.viewDocs:
+                toDocuments();
+                break;
+            case R.id.savebutton:
+                break;
 
         }
     }
+
+    private void setUpGenderSetter() {
+
+        final ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this,
+                R.array.Genders, android.R.layout.simple_spinner_dropdown_item);
+
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSetter.setAdapter(genderAdapter);
+
+        genderSetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position) {
+                    case 0:
+                        //female
+                        break;
+                    case 1:
+                        //male
+                        break;
+                }
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+    private void setUpEthnicitySetter() {
+
+        final ArrayAdapter<CharSequence> ethnicityAdapter = ArrayAdapter.createFromResource(this,
+                R.array.Ethnicity, android.R.layout.simple_spinner_dropdown_item);
+
+        ethnicityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ethnicitySetter.setAdapter(ethnicityAdapter);
+
+        ethnicitySetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position) {
+                    case 0:
+                        //American indian
+                        break;
+                    case 1:
+                        //asian
+                        break;
+                    case 2:
+                        //African american
+                        break;
+                    case 3:
+                        //hispanic
+                        break;
+                    case 4:
+                        //pacific islander
+                        break;
+                    case 5:
+                        //white
+                        break;
+                }
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+    private void toHomepage() {
+        Intent i = new Intent(getBaseContext(), HomepageActivity.class);
+        startActivity(i);
+    }
+
+    private void toDocuments() {
+        Intent i = new Intent(getBaseContext(), HomepageActivity.class);
+        startActivity(i);
+    }
+
 
 }
