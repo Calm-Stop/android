72c57e9b06ee08938ffd94151852eb486ec872a6
Maria Vizcaino <mvizcaino2@gmail.com>
2017-02-20 17:28:04 -0800

    more on the profile page

--- CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ProfileActivity.java
+++ CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ProfileActivity.java
@@ -2,13 +2,22 @@ package com.policestrategies.calm_stop.citizen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
 
 import com.policestrategies.calm_stop.R;
 
@@ -20,6 +29,21 @@ import com.policestrategies.calm_stop.R;
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner genderSetter;
     private Spinner ethnicitySetter;
+    private EditText mname;
+    private EditText memail;
+    private EditText mphoneNum;
+
+    private int gen;
+    private int eth;
+
+    private String Name;
+    private String Email;
+    private String PhoneNumber;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String TAG = "Profile Edit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +52,12 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         findViewById(R.id.backbutton).setOnClickListener(this);
         findViewById(R.id.viewDocs).setOnClickListener(this);
+
         findViewById(R.id.savebutton).setOnClickListener(this);
+
+        mname = (EditText)findViewById(R.id.editName);
+        memail = (EditText)findViewById(R.id.editEmail);
+        mphoneNum = (EditText)findViewById(R.id.editPhonenum);
 
         genderSetter = (Spinner) findViewById(R.id.genderSetter);
         ethnicitySetter = (Spinner) findViewById(R.id.ethnicitySetter);
@@ -36,12 +65,22 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         setUpGenderSetter();
         setUpEthnicitySetter();
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            Name = user.getDisplayName();
+            Email = user.getEmail();
+            //Uri photoUrl = user.getPhotoUrl();
+            //String uid = user.getUid();
+        }
+
+        mname.setText(Name);
+        memail.setText(Email);
 
     }
 
     @Override
     public void onClick(View v) {
-
         switch(v.getId()) {
 
             case R.id.backbutton:
@@ -49,11 +88,18 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 break;
 
             case R.id.viewDocs:
+                //FIXME
                 toDocuments();
                 break;
-            case R.id.savebutton:
-                break;
 
+            case R.id.savebutton:
+                //FIXME
+                //TODO
+                //WRITE TO FIREBASE
+                Name = mname.getText().toString();
+                Email = memail.getText().toString();
+                PhoneNumber = mphoneNum.getText().toString();
+                break;
         }
     }
 
@@ -71,9 +117,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 switch(position) {
                     case 0:
                         //female
+                        gen = 0;
                         break;
                     case 1:
                         //male
+                        gen = 1;
                         break;
                 }
             }
@@ -97,21 +145,27 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 switch(position) {
                     case 0:
                         //American indian
+                        eth = 0;
                         break;
                     case 1:
                         //asian
+                        eth = 1;
                         break;
                     case 2:
                         //African american
+                        eth = 2;
                         break;
                     case 3:
                         //hispanic
+                        eth = 3;
                         break;
                     case 4:
                         //pacific islander
+                        eth = 4;
                         break;
                     case 5:
                         //white
+                        eth = 5;
                         break;
                 }
             }
