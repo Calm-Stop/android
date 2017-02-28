3a015f4fb4583adf470a1c927db520d8fcb45c3a
Maria Vizcaino <mvizcaino2@gmail.com>
2017-02-27 16:12:48 -0800

    Added activities for Officer Android App

---
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/HistoryActivity.java
@@ -0,0 +1,33 @@
+package com.policestrategies.calm_stop.officer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.policestrategies.calm_stop.R;
+
+/**
+ * Created by mariavizcaino on 2/27/17.
+ */
+
+public class HistoryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+/*
+            case R.id.button_logout: // The login button was pressed - let's run the login function
+                logout();
+                break;
+*/
+        }
+    }
+
+}
