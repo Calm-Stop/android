ac73207870e06814df56711b897d42b7a4c33ca3
Howard Ting <cuberuru@gmail.com>
2017-02-27 18:51:02 -0800

    Officer SignupForm now takes in variety of fields and stores it under department number field input

--- CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/Citizen.java
+++ CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/Citizen.java
@@ -5,26 +5,26 @@ package com.policestrategies.calm_stop.citizen;
  */
 
 public class Citizen {
-        String email, password, licensenum,
+    String email, password, licensenum,
                 firstname, lastname, phone, address, gender, language, dateofbirth;
 
-        public Citizen(String email, String password, String licenseNum, String firstname,
-                       String lastname, String phone, String address, String gender, String language,
-                       String dateofbirth) {
-            this.email = email;
-            this.password = password;
-            this.licensenum = licenseNum;
-            this.firstname = firstname;
-            this.lastname = lastname;
-            this.phone = phone;
-            this.address = address;
-            this.gender = gender;
-            this.language = language;
-            this.dateofbirth = dateofbirth;
-        }
-
-        public Citizen(final String email, final String password) {
-            this.email = email;
-            this.password = password;
-        }
+    public Citizen(String email, String password, String licenseNum, String firstname,
+                   String lastname, String phone, String address, String gender, String language,
+                   String dateofbirth) {
+        this.email = email;
+        this.password = password;
+        this.licensenum = licenseNum;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.address = address;
+        this.gender = gender;
+        this.language = language;
+        this.dateofbirth = dateofbirth;
     }
+
+    public Citizen(final String email, final String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
