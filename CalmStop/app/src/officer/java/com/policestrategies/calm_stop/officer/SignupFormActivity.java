f5cc29bae48afd7766b8330254583b4707f504f0
Howard Ting <cuberuru@gmail.com>
2017-03-09 14:45:43 -0800

    import signupverification into officer signupform and merge master

--- CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupFormActivity.java
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/SignupFormActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.policestrategies.calm_stop.R;
+import com.policestrategies.calm_stop.SignupVerification;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
