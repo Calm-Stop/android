090488de7fde789e7d572ee08bd062e7077a9138
Howard Ting <cuberuru@gmail.com>
2017-03-09 19:04:02 -0800

    untested change to date of birth regex which parses DOB input to 3 ints which can be checked to have valid numerical inputs; unit test more comprehensive

--- CalmStop/app/src/main/java/com/policestrategies/calm_stop/SignupVerification.java
+++ CalmStop/app/src/main/java/com/policestrategies/calm_stop/SignupVerification.java
@@ -28,6 +28,7 @@ public class SignupVerification {
         return (phone.matches("^(1|(1(-|\\s)))?(\\d{10}|" + "\\(?\\d{3}\\)?(-|\\s)?\\d{3}(-|\\s)?\\d{4})\\s?"));
     }
 
+
     public static boolean validFirstName(String firstname) {
         return (!firstname.isEmpty());
     }
@@ -37,7 +38,39 @@ public class SignupVerification {
     }
 
     public static boolean validDateOfBirth(String dateofbirth) {
-        return (dateofbirth.matches("^\\d{1,2}(-|(,\\s)|/)\\d{1,2}(-|(,\\s)|/)\\d{4}"));
+        if (dateofbirth.matches("^\\d{1,2}-\\d{1,2}-\\d{4}")) {
+            int i,j,k,l;
+            String S_day="", S_month="", S_year="";
+            int I_day, I_month, I_year;
+            String dummy = dateofbirth.substring(0,1);
+            for (i = 0, j = 1; !dummy.equalsIgnoreCase("-");) {
+                S_day = dateofbirth.substring(0,j);
+                i++; j++;
+                dummy = dateofbirth.substring(i,j);
+            }
+            //i,j = (1,2)|(2,3)
+            k = j;
+            i=j+1;
+            dummy = dateofbirth.substring(j,i);
+            while (!dummy.equalsIgnoreCase("-")) {
+                S_month = dateofbirth.substring(k,i);
+                i++; j++;
+                dummy = dateofbirth.substring(j,i);
+            }
+            k=i;
+            j=i+1;
+            dummy = dateofbirth.substring(i,j);
+            while (!dummy.equalsIgnoreCase("-")) {
+                S_year = dateofbirth.substring(k,j);
+                i++; j++;
+                dummy = dateofbirth.substring(i,j);
+            }
+            I_day = Integer.parseInt(S_day);
+            I_month = Integer.parseInt(S_month);
+            I_year = Integer.parseInt(S_year);
+            if (I_day > 31 || I_month > 12 || I_year < 1900 || I_year > 2010) return false;
+        }
+        return true;
     }
 
 } // end class SignupVerification
--- CalmStop/app/src/test/java/com/policestrategies/calm_stop/SignupVerificationTest.java
+++ CalmStop/app/src/test/java/com/policestrategies/calm_stop/SignupVerificationTest.java
@@ -15,10 +15,46 @@ public class SignupVerificationTest {
         String testEmail = "";
         Assert.assertFalse(SignupVerification.validEmail(testEmail));
     }
-
+//Password Tests
     @Test
     public void testEmptyPassword() {
         String testPassword = "";
+        Assert.assertFalse(SignupVerification.validPassword(testPassword));
+    }
+
+    @Test
+    public void testNumericPassword() {
+        String testPassword = "123456";
+        Assert.assertFalse(SignupVerification.validPassword(testPassword));
+    }
+
+    @Test
+    public void testSmallPassword() {
+        String testPassword = "12345";
+        Assert.assertFalse(SignupVerification.validPassword(testPassword));
+    }
+
+    @Test
+    public void testAlphaPassword() {
+        String testPassword = "ABCDEFG";
+        Assert.assertFalse(SignupVerification.validPassword(testPassword));
+    }
+
+    @Test
+    public void testValidPassword() {
+        String testPassword = "A12345";
+        Assert.assertTrue(SignupVerification.validPassword(testPassword));
+    }
+//License Tests
+    @Test
+    public void testValidLicense() {
+        String testLicense = "A12345678";
+        Assert.assertTrue(SignupVerification.validLicense(testLicense));
+    }
+
+    @Test
+    public void testFiveCharPassword() {
+        String testPassword = "12345";
         Assert.assertFalse(SignupVerification.validPassword(testPassword));
     }
 
@@ -29,15 +65,64 @@ public class SignupVerificationTest {
     }
 
     @Test
+    public void testNumericLicense() {
+        String testLicense = "123456789";
+        Assert.assertFalse(SignupVerification.validLicense(testLicense));
+    }
+
+    @Test
+    public void testAlphaLicense() {
+        String testLicense = "ABCDEFGHI";
+        Assert.assertFalse(SignupVerification.validLicense(testLicense));
+    }
+
+    @Test
+    public void testMultiAlphaLicense() {
+        String testLicense = "AB1234567";
+        Assert.assertFalse(SignupVerification.validLicense(testLicense));
+    }
+
+    @Test
+    public void testSmallLicense() {
+        String testLicense = "A1234567";
+        Assert.assertFalse(SignupVerification.validLicense(testLicense));
+    }
+//Address Tests
+    @Test
     public void testEmptyAddress() {
         String testAddress = "";
         Assert.assertFalse(SignupVerification.validAddress(testAddress));
     }
 
+//Phone Tests
     @Test
     public void testEmptyPhone() {
         String testPhone = "";
         Assert.assertFalse(SignupVerification.validPhone(testPhone));
+    }
+
+    @Test
+    public void testParensPhone() {
+        String testPhone = "(800)-666-6666";
+        Assert.assertTrue(SignupVerification.validPhone(testPhone));
+    }
+
+    @Test
+    public void testNoParensPhone() {
+        String testPhone = "800-666-6666";
+        Assert.assertTrue(SignupVerification.validPhone(testPhone));
+    }
+
+    @Test
+    public void testNoAreaCodePhone() {
+        String testPhone = "6666666";
+        Assert.assertFalse(SignupVerification.validPhone(testPhone));
+    }
+
+    @Test
+    public void testFullInputPhone() {
+        String testPhone = "1-(408)-666-6666";
+        Assert.assertTrue(SignupVerification.validPhone(testPhone));
     }
 
     @Test
@@ -52,12 +137,41 @@ public class SignupVerificationTest {
         Assert.assertFalse(SignupVerification.validLastName(testLastName));
     }
 
+//DOB Test
     @Test
     public void testEmptyDateOfBirth() {
         String testDateOfBirth = "";
         Assert.assertFalse(SignupVerification.validDateOfBirth(testDateOfBirth));
     }
 
+    @Test
+    public void testMinimalDateOfBirth() {
+        String testDateOfBirth = "1/2/1999";
+        Assert.assertTrue(SignupVerification.validDateOfBirth(testDateOfBirth));
+    }
 
+    @Test
+    public void testLongDateOfBirth() {
+        String testDateOfBirth = "100/200/19999";
+        Assert.assertFalse(SignupVerification.validDateOfBirth(testDateOfBirth));
+    }
+
+    @Test
+    public void testShortDateOfBirth() {
+        String testDateOfBirth = "1/2/999";
+        Assert.assertFalse(SignupVerification.validDateOfBirth(testDateOfBirth));
+    }
+
+    @Test
+    public void testZeroDateOfBirth() {
+        String testDateOfBirth = "00/00/0000";
+        Assert.assertFalse(SignupVerification.validDateOfBirth(testDateOfBirth));
+    }
+
+    @Test
+    public void testNineDateOfBirth() {
+        String testDateOfBirth = "99/99/9999";
+        Assert.assertFalse(SignupVerification.validDateOfBirth(testDateOfBirth));
+    }
 
 } // end SignupVerificationTest
