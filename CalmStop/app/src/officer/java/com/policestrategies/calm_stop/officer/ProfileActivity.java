904a68e5642a0192486026258af9392f7bd98359
talal.abouhaiba <talal.abouhaiba@gmail.com>
2017-04-07 18:04:30 -0700

    Fixed Issue #31 - Officer profile page working

--- CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/ProfileActivity.java
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/ProfileActivity.java
@@ -35,35 +35,25 @@ import com.policestrategies.calm_stop.R;
 import com.policestrategies.calm_stop.RegexChecks;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
-    private Spinner daySetter;
-    private Spinner monthSetter;
-    private Spinner yearSetter;
     private Spinner genderSetter;
-    private Spinner ethnicitySetter;
-    private Spinner languageSetter;
     private EditText mFirstNameField;
     private EditText mLastNameField;
     private EditText mEmailField;
-    private EditText mPhone;
-    private TextView mDateOfBirth;
 
-    private EditText mZip;
-    private EditText mLicense;
+    private String mDepartmentNumber;
+
     private EditText mDepartmentNum;
     private EditText mBadgeNum;
     private ImageView mphoto;
 
     private static final int chosenImage = 1;
 
-    private int i_year, i_day, i_month, i_ethnicity, i_language, i_gender;
+    private int i_gender;
 
     private String FName;
     private String LName;
     private String Email;
-    private String PhoneNumber;
 
-    private String License;
-    private String ZIP;
     private String Department;
     private String Badge;
 
@@ -83,16 +73,16 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mDepartmentNumber = getSharedPreferences(getString(R.string.shared_preferences),
+                MODE_PRIVATE).getString(getString(R.string.shared_preferences_department_number),
+                "");
+
         //mFname, mLname, memail, mDOB, mphoneNum, mLicense, mZip, mLanguage, mphoto
         mFirstNameField = (EditText)findViewById(R.id.editFirstName);
         mLastNameField = (EditText)findViewById(R.id.editLastName);
         mEmailField = (EditText)findViewById(R.id.editEmail);
-        mPhone = (EditText)findViewById(R.id.editPhonenum);
-        mLicense= (EditText)findViewById(R.id.editLicenseNumber);
-        mZip = (EditText)findViewById(R.id.editZip);
         mDepartmentNum = (EditText)findViewById(R.id.editdepartnum);
         mBadgeNum = (EditText)findViewById(R.id.editBadgenum);
-        mDateOfBirth = (TextView) findViewById(R.id.dateOfBirth);
 
         mphoto = (ImageView)findViewById(R.id.profilePicture);
         mphoto.setOnClickListener(this);
@@ -101,18 +91,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         findViewById(R.id.savebutton).setOnClickListener(this);
 
         genderSetter = (Spinner) findViewById(R.id.genderSetter);
-        ethnicitySetter = (Spinner) findViewById(R.id.ethnicitySetter);
-        languageSetter = (Spinner) findViewById(R.id.languageSetter);
-        monthSetter = (Spinner) findViewById(R.id.monthSetter);
-        daySetter = (Spinner) findViewById(R.id.daySetter);
-        yearSetter = (Spinner) findViewById(R.id.yearSetter);
 
         setUpGenderSetter();
-        setUpEthnicitySetter();
-        setUpLanguageSetter();
-        setUpMonthSetter();
-        setUpDaySetter();
-        setUpYearSetter();
 
         user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -124,7 +104,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         }
 
         mDatabase = FirebaseDatabase.getInstance().getReference("officer");
-        profileRef = mDatabase.child(officerUid).child("profile");
+        profileRef = mDatabase.child(mDepartmentNumber).child(officerUid).child("profile");
 
         Photo = user.getPhotoUrl();
 
@@ -138,20 +118,12 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 //NEED TO ADD BADGE NUMBER
-                ZIP = snapshot.child("zip_code").getValue().toString();
                 Email = snapshot.child("email").getValue().toString();
                 FName = snapshot.child("first_name").getValue().toString();
                 LName = snapshot.child("last_name").getValue().toString();
-                License = snapshot.child("license_number").getValue().toString();
-                PhoneNumber = snapshot.child("phone_number").getValue().toString();
                 Department = snapshot.child("department").getValue().toString();
                 Badge = snapshot.child("badge").getValue().toString();
                 i_gender = Integer.parseInt(snapshot.child("gender").getValue().toString());
-                i_language = Integer.parseInt(snapshot.child("language").getValue().toString());
-                i_ethnicity = Integer.parseInt(snapshot.child("ethnicity").getValue().toString());
-                i_day = Integer.parseInt(snapshot.child("day").getValue().toString());
-                i_month = Integer.parseInt(snapshot.child("month").getValue().toString());
-                i_year = Integer.parseInt(snapshot.child("year").getValue().toString());
                 setEverything();
             }
 
@@ -176,24 +148,16 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 FName = mFirstNameField.getText().toString();
                 LName = mLastNameField.getText().toString();
                 Email = mEmailField.getText().toString();
-                PhoneNumber = mPhone.getText().toString();
-                License = mLicense.getText().toString();
-                ZIP = mZip.getText().toString();
                 Department = mDepartmentNum.getText().toString();
                 Badge = mBadgeNum.getText().toString();
 
-                if (!validateInput(FName, LName, Department, Badge, Email, PhoneNumber, ZIP, License)) return;
+                if (!validateInput(FName, LName, Department, Badge, Email)) return;
                 //WRITE TO FIREBASE
                 updateFName();
                 updateLName();
                 updatePhoto();
-                updatePhoneNumber();
                 updateEmail();
-                updateZip();
-                updateLicense();
-                updateLanguage();
                 updateGender();
-                updateEthnicity();
                 updateDepartment();
                 updateBadge();
                 updateDOB();
@@ -222,55 +186,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
-    private void setUpDaySetter() {
-        final ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter.createFromResource(this,
-                R.array.Day_31, android.R.layout.simple_spinner_dropdown_item);
-        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        daySetter.setAdapter(dayAdapter);
-        daySetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                i_day = position;
-            }
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
 
-    }
-
-    private void setUpYearSetter() {
-        final ArrayAdapter<CharSequence> yearAdapter = ArrayAdapter.createFromResource(this,
-                R.array.Year, android.R.layout.simple_spinner_dropdown_item);
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        yearSetter.setAdapter(yearAdapter);
-        yearSetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                i_year = position;
-            }
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
-
-
-    private void setUpMonthSetter() {
-        final ArrayAdapter<CharSequence> monthAdapter = ArrayAdapter.createFromResource(this,
-                R.array.Month, android.R.layout.simple_spinner_dropdown_item);
-        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        monthSetter.setAdapter(monthAdapter);
-        monthSetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                i_month = position;
-            }
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
 
 
     private void setUpGenderSetter() {
@@ -292,43 +208,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         });
     }
 
-    private void setUpEthnicitySetter() {
-
-        final ArrayAdapter<CharSequence> ethnicityAdapter = ArrayAdapter.createFromResource(this,
-                R.array.Ethnicity, android.R.layout.simple_spinner_dropdown_item);
-
-        ethnicityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ethnicitySetter.setAdapter(ethnicityAdapter);
-
-        ethnicitySetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                i_ethnicity = position;
-            }
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
-
-    private void setUpLanguageSetter() {
-
-        final ArrayAdapter<CharSequence> languageAdapter = ArrayAdapter.createFromResource(this,
-                R.array.Language, android.R.layout.simple_spinner_dropdown_item);
-
-        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        languageSetter.setAdapter(languageAdapter);
-
-        languageSetter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                i_language = position;
-            }
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-            }
-        });
-    }
 
     private void setEverything() {
 
@@ -337,15 +216,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         mDepartmentNum.setText(Department);
         mBadgeNum.setText(Badge);
         mEmailField.setText(Email);
-        mPhone.setText(PhoneNumber);
-        mZip.setText(ZIP);
-        mLicense.setText(License);
         genderSetter.setSelection(i_gender);
-        ethnicitySetter.setSelection(i_ethnicity);
-        languageSetter.setSelection(i_language);
-        monthSetter.setSelection(i_month);
-        daySetter.setSelection(i_day);
-        yearSetter.setSelection(i_year);
     }
 
     private void authentication() {
@@ -437,19 +308,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private void updateFName() { profileRef.child("first_name").setValue(FName); }
     private void updateLName() { profileRef.child("last_name").setValue(LName); }
-    private void updatePhoneNumber() { profileRef.child("phone_number").setValue(PhoneNumber); }
-    private void updateZip() {profileRef.child("zip_code").setValue(ZIP);}
-    private void updateLicense() { profileRef.child("license_number").setValue(License); }
     private void updateDepartment() { profileRef.child("department").setValue(Department); }
     private void updateBadge() { profileRef.child("badge").setValue(Badge); }
     private void updateDOB() {
-        //DATE OF BIRTH
-        profileRef.child("day").setValue(i_day);
-        profileRef.child("month").setValue(i_month);
-        profileRef.child("year").setValue(i_year);
     }
-    private void updateLanguage() { profileRef.child("language").setValue(i_language); }
-    private void updateEthnicity() { profileRef.child("ethnicity").setValue(i_ethnicity); }
     private void updateGender() { profileRef.child("gender").setValue(i_gender); }
 
 
@@ -460,8 +322,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         startActivity(i);
     }
 
-    private boolean validateInput(String firstname, String lastname, String department, String badge, String email,
-                                  String phone, String zip, String license) {
+    private boolean validateInput(String firstname, String lastname, String department, String badge, String email
+                                  ) {
 
 //FName, LName, Department, Badge, Email, PhoneNumber, ZIP, License, DOB
         //FIRST NAME CHECK
@@ -508,43 +370,6 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             mEmailField.setError(null);
         }
 
-        //PHONE REGEX
-        if(!RegexChecks.validPhone(phone)) {
-            mPhone.setError("Invalid Phone Number.");
-            mPhone.requestFocus();
-            return false;
-        } else {
-            mPhone.setError(null);
-        }
-
-        //ZIP CODE REGEX - 5 DIGIT NUMBER
-        if(!RegexChecks.validZip(zip)) {
-            mZip.setError("Invalid ZIP");
-            mZip.requestFocus();
-            return false;
-        } else {
-            mZip.setError(null);
-        }
-
-        //DRIVER'S LICENSE REGEX (CALIFORNIA FORMAT)
-        if(!RegexChecks.validLicense(license)) {
-            mLicense.setError("Enter a letter followed by eight numbers\nExample: A12345678");
-            mLicense.requestFocus();
-            return false;
-        } else {
-            mLicense.setError(null);
-        }
-
-        //DATE OF BIRTH REGEX; replace with spinner for month, day, and year.
-        mDateOfBirth.clearFocus();
-        if (!RegexChecks.validDateOfBirth(i_month, i_day, getResources().getStringArray(R.array.Year).length + 1909 - i_year)){
-            mDateOfBirth.setError("Invalid Date of Birth");
-            mDateOfBirth.requestFocus();
-            return false;
-        } else {
-            mDateOfBirth.setError(null);
-        }
         return true;
-
     }
-}
+} // end ProfileActivity
