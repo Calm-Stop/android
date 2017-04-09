a1f25deab7a3348a88adb5a64c16a6d1b2fe993d
Howard Ting <cuberuru@gmail.com>
2017-04-09 12:46:38 -0700

    Empty chat UI; for testing purposes, the root AndroidManifest is configured to jump to chatactivity as its on-launch activity

--- CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ChatActivity.java
+++ CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ChatActivity.java
@@ -9,31 +9,14 @@ import android.widget.EditText;
 
 import com.policestrategies.calm_stop.R;
 
-public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-    private Button send;
-    private EditText content;
-
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.backbutton:
-                toHomepage();
-                break;
-
-        }
-
-    }
-
-    private void toHomepage() {
-        Intent i = new Intent(getBaseContext(), HomepageActivity.class);
-        startActivity(i);
     }
 }
