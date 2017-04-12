201bad6aed013ec343ff5aa13e0e1a51dc0358a1
Howard Ting <cuberuru@gmail.com>
2017-04-12 16:31:21 -0700

    the skeleton for firebase calls are laid out; if userID can be retrieved; threadID and the context for generating it is unaccounted for and ignored

--- CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ChatActivity.java
+++ CalmStop/app/src/citizen/java/com/policestrategies/calm_stop/citizen/ChatActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
@@ -11,15 +12,22 @@ import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import com.google.firebase.database.ValueEventListener;
 import com.policestrategies.calm_stop.ChatArrayAdapter;
 import com.policestrategies.calm_stop.ChatMessage;
 import com.policestrategies.calm_stop.R;
+
+import static java.lang.System.currentTimeMillis;
 
 public class ChatActivity extends Activity {
     private static final String TAG = "ChatActivity";
@@ -32,15 +40,19 @@ public class ChatActivity extends Activity {
     private DatabaseReference databaseRef;
     private FirebaseAuth mAuth;
     private DatabaseReference threadReference;
+    private DatabaseReference messagesReference;
     private DatabaseReference userProfileReference;
-    private String UserID;
+    private String authorID;
+    private String threadID;
     private String name;
+    private long timestamp;
 
     Intent intent;
     private boolean side = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         Intent i = getIntent();
         setContentView(R.layout.activity_chat);
@@ -48,24 +60,81 @@ public class ChatActivity extends Activity {
         buttonSend = (Button) findViewById(R.id.button_send);
         listView = (ListView) findViewById(R.id.listView1);
 
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.activity_chat_singlemessage);
+        listView.setAdapter(chatArrayAdapter);
+//Default ChatMessage object values to be passed to ChatMessage constructor
+        chatText = (EditText) findViewById(R.id.chat_text);
+        name = "[Default_Name]:";
+        timestamp = System.currentTimeMillis();
+        authorID = threadID = "000000000000";
+        side = false;
+
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+        final FirebaseUser user = mAuth.getCurrentUser();
+
+//FIREBASE CODE
         if (user != null) {
-            UserID = user.getUid();
+//IF DATABASE ACCESSIBLE:
+            //Set References to thread and user
+            authorID = user.getUid();
             databaseRef = FirebaseDatabase.getInstance().getReference();
             threadReference = databaseRef.child("stops").child("temp_stop_id")
                     .child("thread").getRef();
             userProfileReference = databaseRef.child("citizen")
-                    .child(UserID).child("profile").getRef();
+                    .child(authorID).child("profile").getRef();
+            messagesReference = threadReference.child("messages").getRef();
             name = userProfileReference.child("first_name").toString();
-        } else {
-            UserID = "Random UserID String (placeholder)";
-            name = "[Sender_Name]:";
-        }
+            //Listen for changes to chat
+            ChildEventListener childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "In Citizen/ChatActivity, onChildAdded");
+                    ChatMessage cmessage = dataSnapshot.getValue(ChatMessage.class);
+                    if (cmessage.getAuthorID().equals(authorID)) {
+                        cmessage.left = false;
+                    } else {
+                        cmessage.left = true;
+                    }
+                    chatArrayAdapter.add(cmessage);
+                    chatText.setText("");
+                    // ...
+                }
 
-        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.activity_chat_singlemessage);
-        listView.setAdapter(chatArrayAdapter);
-        chatText = (EditText) findViewById(R.id.chat_text);
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+                    //Comment newComment = dataSnapshot.getValue(Comment.class);
+                    String Key = dataSnapshot.getKey();
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+                    String Key = dataSnapshot.getKey();
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+                    //Comment movedComment = dataSnapshot.getValue(Comment.class);
+                    //String commentKey = dataSnapshot.getKey();
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                    Toast.makeText(getBaseContext(), "Failed to load comments.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            };
+
+            messagesReference.addChildEventListener(childEventListener);
+//END DATABASE CODE
+        }
+//END FIREBASE CODE
+
+        //ON KEY PRESS? WHY?
         chatText.setOnKeyListener(new OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
@@ -74,10 +143,20 @@ public class ChatActivity extends Activity {
                 return false;
             }
         });
+        //ALSO SEND TO FIREBASE
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                //setting newMessage fields
+                timestamp = currentTimeMillis();
+                side = false;
+                //The context for generating threadID has yet to be designed or implemented
+                //creating new message from set fields
+                ChatMessage newMessage = new ChatMessage(side, chatText.getText().toString(),
+                        timestamp, threadID, authorID, name);
+                //sendToFirebase(newMessage);
                 sendChatMessage();
+                //SEND MESSAGE TO FIREBASE INSTEAD
             }
         });
 
@@ -96,11 +175,27 @@ public class ChatActivity extends Activity {
 
     private boolean sendChatMessage(){
         //if user_ID == authorID, set side = false; else set side equal true
+        ChatMessage newMessage = new ChatMessage(side,
+                chatText.getText().toString(), 0, threadID, authorID, name);
         side = false; //output on the right side
-        chatArrayAdapter.add(new ChatMessage(side,
-                chatText.getText().toString(), 0, UserID, name));
+        chatArrayAdapter.add(newMessage);
         chatText.setText("");
         return true;
     }
 
+    //A message is sent to firebase, triggering onChildEvent.
+    //chatArrayAdapter will update only on firebase change.
+    //This change will only occur on the following:
+    //1. a message is received from firebase
+    //2. a message is sent to firebase
+    //in no other case should chatarrayadapter receive messages
+    private void sendToFirebase(ChatMessage newMessage){
+        //if user_ID == authorID, set side = false; else set side equal true
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            //push a new message onto message tree:
+            DatabaseReference newMessagesRef = messagesReference.push(); //push a unique message ID string
+            newMessagesRef.setValue(newMessage); //store corresponding message content under the key
+        }
+    }
 }
--- CalmStop/app/src/main/java/com/policestrategies/calm_stop/ChatMessage.java
+++ CalmStop/app/src/main/java/com/policestrategies/calm_stop/ChatMessage.java
@@ -3,7 +3,8 @@ package com.policestrategies.calm_stop;
 public class ChatMessage {
 //FIELDS
     private String content;
-    private int timestamp;
+    private long timestamp;
+    private String threadID;
     private String authorID;
     private String recipientID;
     private String authorName;
@@ -11,12 +12,13 @@ public class ChatMessage {
 
 //CONSTRUCTOR
 
-    public ChatMessage(boolean left, String content, int timestamp,
-                       String authorID, String authorName) {
+    public ChatMessage(boolean left, String content, long timestamp,
+                       String threadID, String authorID, String authorName) {
         super();
         this.left = left;
         this.content = content;
         this.timestamp = timestamp;
+        this.threadID = threadID;
         this.authorID = authorID;
         this.authorName = authorName;
     }
@@ -27,12 +29,12 @@ public class ChatMessage {
         this.content = content;
     }
 
-    //GETTERS
-    public int getTimestamp() { return timestamp; }
-
+    //GETTERS for private functions (no setters)
     public String getContent() { return content; }
 
     public String getAuthorID() { return authorID; }
 
     public String getAuthorName() { return authorName; }
+
+    public String getThreadID() { return threadID; }
 } // end class ChatMessage
