4c17ffc2367e836ae7b13119746490debde900de
talal.abouhaiba <talal.abouhaiba@gmail.com>
2017-04-28 00:14:13 -0700

    Partial Issue #64 - Officer image and name being presented on dashboard

--- CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/DashboardActivity.java
+++ CalmStop/app/src/officer/java/com/policestrategies/calm_stop/officer/DashboardActivity.java
@@ -6,22 +6,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.policestrategies.calm_stop.R;
 import com.policestrategies.calm_stop.SharedUtil;
 import com.policestrategies.calm_stop.officer.beacon_registration.BeaconRegistrationActivity;
 
 /**
+ * Officer landing page. From here, an officer can begin a traffic stop.
  * @author Talal Abou Haiba
  */
 
@@ -29,8 +35,10 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
     private BottomNavigationView mBottomNavigationView;
     private ProgressDialog mProgressDialog;
+    private ImageView mProfileImageView;
 
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseReference;
+    private StorageReference mStorageReference;
 
     private String mUid;
     private String mDepartmentNumber;
@@ -41,13 +49,19 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         mProgressDialog = ProgressDialog.show(this, "", "Loading", true, false);
 
         findViewById(R.id.button_manage_beacon).setOnClickListener(this);
         findViewById(R.id.button_make_stop).setOnClickListener(this);
 
+        mProfileImageView = ((ImageView) findViewById(R.id.dashboard_officer_profile_picture));
         mDepartmentNumber = Utility.getCurrentDepartmentNumber(this);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mStorageReference = FirebaseStorage.getInstance().getReference();
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() != null && !mDepartmentNumber.isEmpty()) {
@@ -59,7 +73,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
             startActivity(i);
         }
 
-        obtainCurrentBeaconInfo();
+        loadCurrentProfile();
 
         mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         setBottomNavigationView();
@@ -90,9 +104,42 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    private void loadCurrentProfile() {
+
+        final DatabaseReference profileReference;
+        profileReference = FirebaseDatabase.getInstance().getReference("officer")
+                .child(mDepartmentNumber).child(mUid).child("profile");
+
+        profileReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String lastName = snapshot.child("last_name").getValue().toString();
+                String photoPath = snapshot.child("photo").getValue().toString();
+                ((TextView) findViewById(R.id.dashboard_officer_welcome))
+                        .setText(getResources().getString(R.string.officer_welcome, lastName));
+
+                profileReference.child("photo").child(photoPath);
+
+                Glide.with(DashboardActivity.this)
+                        .using(new FirebaseImageLoader())
+                        .load(mStorageReference.child(photoPath))
+                        .into(mProfileImageView);
+
+                SharedUtil.dismissProgressDialog(mProgressDialog);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.e("", "Failed to read app title value.", error.toException());
+            }
+
+        });
+
+    }
+
     private void obtainCurrentBeaconInfo() {
 
-        DatabaseReference officerProfileReference = mDatabase.child("officer")
+        DatabaseReference officerProfileReference = mDatabaseReference.child("officer")
                 .child(mDepartmentNumber).child(mUid).child("profile").getRef();
 
         officerProfileReference.addListenerForSingleValueEvent(new ValueEventListener() {
